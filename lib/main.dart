b8c0bf007496738deaa51008f38da58aaa47f6fc
motman <tandemnet@icloud.com>
2025-12-15 23:00:09 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'splash_screen.dart';
